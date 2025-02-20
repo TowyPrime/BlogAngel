045dad534536f263ee626bc35b9bc6e9fcfd4d71
TowyPrime <angeldti12@gmail.com>
2025-02-20 14:27:48 -0600

    Modificar-todo

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnNavEl = document.querySelector('.btn-mobile-menu');
+const headerNav = document.querySelector('.menu-nav');
+
+btnNavEl.addEventListener('click', () =>{
+    headerNav.classList.toggle('nav-activo');
+})
